1171565e7dac455ccc0b0d527feeb2039fc33782
Vitaliy Shapovalov <kupper133@gmail.com>
2020-09-18 18:07:32 +0300

    init: project files

---
+++ objc-concentration-game/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  objc-concentration-game
+//
+//  Created by Виталий Шаповалов on 18.09.2020.
+//  Copyright © 2020 Виталий Шаповалов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ objc-concentration-game/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  objc-concentration-game
+//
+//  Created by Виталий Шаповалов on 18.09.2020.
+//  Copyright © 2020 Виталий Шаповалов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
